ad57d4688865c184d858dc16a954d4be3d4ccdcb
AlexPrivalov78 <privalov_78@list.ru>
2024-09-29 11:10:11 +0300

    Add files via upload

--- module_1_3.py (Dynamic Typing 26.09).py
+++ module_1_3.py (Dynamic Typing 26.09).py
@@ -2,7 +2,11 @@ name= 'Александр'
 print(name)
 age=46
 print(age)
-New_age=46+1
-print(New_age)
-is_student = True
-print(True)
+#New_age=46+1
+#print(New_age)
+#is_student = True
+#print(True)
+age=age+1
+print(age)
+is_student = name
+print(is_student==name)
