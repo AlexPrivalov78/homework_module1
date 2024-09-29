0a1d124c3707f1b6a242b397cb6f5af6d68bdc3f
AlexPrivalov78 <privalov_78@list.ru>
2024-09-29 17:04:19 +0300

    Add files via upload

---
+++ module_1_5.py (Immutable and mutable objects. Tuples) 29.09.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, False, "Urban"
+print(immutable_var)
+#immutable_var [0] = 400
+print(immutable_var)
+print('элементы кортежа неизменяемы')
+mutable_list = ["module", False, 3, 4]
+mutable_list [1] = 2
+mutable_list [3] = True
+print(mutable_list)
